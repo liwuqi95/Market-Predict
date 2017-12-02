aaf8f3a2f49c165c4625e332e28f58cf53aaa66c
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-12-01 20:36:10 -0500

    data parser for gold

--- Predictor/src/main/scala/SimpleApp.scala
+++ Predictor/src/main/scala/SimpleApp.scala
@@ -1,26 +1,50 @@
 /* SimpleApp.scala */
+
 import org.apache.spark.SparkConf
 
 import org.apache.spark.SparkContext
 
 import org.apache.spark.sql.SparkSession
 
+import org.apache.spark.sql.functions._
+
 object SimpleApp {
   def main(args: Array[String]) {
 
 
+    // Set up Spark running environment
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[4]")
     new SparkContext(conf)
-
-
-
-
-    val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+
+
+    import spark.implicits._
+
+
+    // Open Gold Json file and load data
+    val goldJson = spark.read.json("gold.json")
+
+
+    goldJson.printSchema()
+
+
+
+    val goldData = goldJson.select($"dataset.data")
+
+    val goldDF = goldData.withColumn("data", explode($"data"))
+      .withColumn("date", $"data" (0))
+      .withColumn("open", $"data" (1))
+      .withColumn("close", $"data" (2))
+        .drop("data")
+
+
+
+
+    goldDF.printSchema()
+    goldDF.show(false)
+
+
+
     spark.stop()
   }
 }
