7c3639265a7a3c7d33676a18275a552a95f06b64
Ian <shan.wuzhen@mail.utoronto.ca>
2018-01-10 15:41:51 -0500

    delete orderby when after creating df

--- Predictor/src/main/scala/SimpleApp.scala
+++ Predictor/src/main/scala/SimpleApp.scala
@@ -38,9 +38,8 @@ object SimpleApp {
       .option("mode", "DROPMALFORMED")
       .option("dateFormat", "MM/dd/yyyy ")
       .load("XAUUSD_Candlestick_1_D_BID_01.01.2017-31.12.2017.csv")
-    val pattern = "dd-MMM-yy hh.mm.ss.S a"
-    val newDF = df.orderBy(unix_timestamp(df("Local time"), pattern).cast("timestamp"))
-    newDF.show
+
+   df.show
 
     val gold_indicators = new indicator(df)
 
