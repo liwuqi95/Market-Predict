27a16c5055093d62b56a768fa92ac1f47c27ff08
Ian <shan.wuzhen@mail.utoronto.ca>
2018-01-09 21:14:55 -0500

    Loading gold data from csv file

--- Predictor/src/main/scala/SimpleApp.scala
+++ Predictor/src/main/scala/SimpleApp.scala
@@ -24,16 +24,18 @@ object SimpleApp {
 
     // Open Gold Json file and load data
     val goldJson = spark.read.json("gold.json")
-
-
-
     val goldData = goldJson.select($"dataset.data")
-
     val goldDF = goldData.withColumn("data", explode($"data"))
       .withColumn("date", $"data" (0))
       .withColumn("price", $"data" (1))
       .drop("data")
-
+  // OPen csv file and load gold data
+    val df = spark.read
+      .format("csv")
+      .option("header", "true") //reading the headers
+      .option("mode", "DROPMALFORMED")
+      .load("C:\\496\\Predictor\\XAUUSD_Candlestick_1_D_BID_01.01.2017-31.12.2017.csv")
+      df.show
 
     val gold_indicators = new indicator(goldDF)
 
