6ddbb4d436ebb5c059ceaaf42b502c16401187be
Chengchen Yang <alphonseyang@live.com>
2018-01-09 22:05:59 -0500

    enum for data

--- Predictor/src/main/scala/indicators/indicator.scala
+++ Predictor/src/main/scala/indicators/indicator.scala
@@ -3,6 +3,10 @@ package indicators
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions.asc
 
+object DataTypes extends Enumeration {
+  val closedPrice = 1
+}
+
 
 class indicator(val dataframe: DataFrame) {
 
@@ -33,6 +37,8 @@ class indicator(val dataframe: DataFrame) {
   val cci_indicator = new cci(20)
 
   /** AROON **/
+  //http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:aroon
+  //https://www.investopedia.com/articles/trading/06/aroon.asp
   val aroon_indicator = new aroon(25)
 
 
@@ -51,31 +57,31 @@ class indicator(val dataframe: DataFrame) {
 
       /** SMA **/
       var isSMAUp:Boolean = false
-      isSMAUp = sma_indicator.computeSMAResult(iteration.getString(1).toFloat)
+      isSMAUp = sma_indicator.computeSMAResult(iteration.getString(DataTypes.closedPrice).toFloat)
 
       /** EMA **/
       var isEMAUp:Boolean = false
-      isEMAUp = ema_indicator.computeEMAResult(iteration.getString(1).toFloat)
+      isEMAUp = ema_indicator.computeEMAResult(iteration.getString(DataTypes.closedPrice).toFloat)
 
       /** MACD **/
       var isMACDUp:Boolean = false
-      isMACDUp = macd_indicator.computeMACDResult(iteration.getString(1).toFloat)
+      isMACDUp = macd_indicator.computeMACDResult(iteration.getString(DataTypes.closedPrice).toFloat)
 
       /** RSI **/
       var RSIValue:Float = 50
       // this value is only being considered when it is bigger than 70/80 and lower than 30/20
       // in between, we'll not consider
-      RSIValue = rsi_indicator.computeRSIResult(iteration.getString(1).toFloat)
+      RSIValue = rsi_indicator.computeRSIResult(iteration.getString(DataTypes.closedPrice).toFloat)
 
       /** STOCH **/
       var STOCHValue: Float = 50
       // this value is considered overbought when above 80, oversold when below 20
-      STOCHValue = stoch_indicator.computeSTOCHResult(iteration.getString(1).toFloat)
+      STOCHValue = stoch_indicator.computeSTOCHResult(iteration.getString(DataTypes.closedPrice).toFloat)
 
       /** CCI **/
       var CCIValue: Float = 0
       // this value is considered overbought when above 80, oversold when below 20
-      CCIValue = cci_indicator.computeCCIResult(iteration.getString(1).toFloat)
+      CCIValue = cci_indicator.computeCCIResult(iteration.getString(DataTypes.closedPrice).toFloat)
 
       total_counter += 1
       if (CCIValue >= -100 && CCIValue <= 100)
@@ -85,7 +91,9 @@ class indicator(val dataframe: DataFrame) {
       var AROONValue: (Float, Float) = (50, 50)
       // up(0) >= 70 && down(1) <= 30, bull
       // up(0) <= 30 &7 down(1) >= 70, bear
-      AROONValue = aroon_indicator.computeAROONResult(iteration.getString(1).toFloat)
+      // when 30-70, if up cross above down, bull
+      // if down cross above up, bear
+      AROONValue = aroon_indicator.computeAROONResult(iteration.getString(DataTypes.closedPrice).toFloat)
 
 
       println("SMA: " + isSMAUp + "   EMA: " + isEMAUp + "   MACD: " + isMACDUp + "   RSI: " + RSIValue + "   STOCH: " + STOCHValue
